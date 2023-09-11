ea9e9f2753ef4cf0935e52df9f2c8bfc4be6e931
fredaadifardana <fredadifar@gmail.com>
2023-09-11 14:19:30 +0700

    update

--- object-classification/src/App.tsx
+++ object-classification/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import React from "react";
+import Webcam from "react-webcam";
 function App() {
-  return <></>;
+  return (
+    <>
+      <p>Object Classification</p>
+      <Webcam audio={false}></Webcam>
+    </>
+  );
 }
 
 export default App;
